f432ab7203309444ab47e46eefd1c07986d73934
kalibetre <kalbetre@icloud.com>
2023-02-26 00:05:55 +0300

    add Document model

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    version = models.IntegerField(default=1)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    content = models.JSONField(blank=True)
+    is_published = models.BooleanField(default=False)
+    shared_at = models.DateTimeField(null=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
