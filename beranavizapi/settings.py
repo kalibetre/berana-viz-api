6af26f590fd5c34410f362248701b2b299ba13a3
kalibetre <kalbetre@icloud.com>
2023-02-26 00:37:43 +0300

    add core app

--- beranavizapi/settings.py
+++ beranavizapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'documents',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "core.User"
--- documents/models.py
+++ documents/models.py
@@ -1,10 +1,19 @@
 import uuid
 
+from django.conf import settings
 from django.db import models
 
 
 class Document(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     version = models.IntegerField(default=1)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
