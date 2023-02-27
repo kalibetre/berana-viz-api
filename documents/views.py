ae799c0e05122615a5b2632ab621e5527734c748
kalibetre <kalbetre@icloud.com>
2023-02-27 21:55:19 +0300

    add a document view set

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Document
+from .serializer import DocumentSerializer
+
+
+class DocumentViewSet(ModelViewSet):
+    """
+    A viewset that provides `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions for Document instances
+    """
+    serializer_class = DocumentSerializer
+    queryset = Document.objects.all()
+    lookup_field = "uid"
+
+    def get_serializer_context(self):
+        return {'request': self.request}
