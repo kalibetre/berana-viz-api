518e20baae2678631aa606d988214a184ab60f16
kalibetre <kalbetre@icloud.com>
2023-02-27 23:12:10 +0300

    create a document owner serializer

--- documents/serializer.py
+++ documents/serializer.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+
+from core.models import User
 
 from .models import Document
 
@@ -12,4 +14,18 @@ class DocumentSerializer(ModelSerializer):
     class Meta:
         model = Document
         fields = ('uid', 'title', 'description', 'modified', 'created',
-                  'content', 'user')
+                  'content')
+
+
+class DocumentOwnerSerializer(ModelSerializer):
+    """
+    A Document Owner serializer that is used to retrieve user information
+    with related documents
+    """
+    documents = PrimaryKeyRelatedField(many=True,
+                                       queryset=Document.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('uid', 'email', 'documents')
+        read_only_fields = ('uid', 'email')
