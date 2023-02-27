c3ef79cdcbd2a9c4b692218db580b2f9da313868
kalibetre <kalbetre@icloud.com>
2023-02-27 23:12:45 +0300

    register document owner viewset to the router

--- documents/urls.py
+++ documents/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import DocumentViewSet
+from .views import DocumentOwnerViewSet, DocumentViewSet
 
 router = SimpleRouter()
 router.register('documents', DocumentViewSet)
+router.register('users', DocumentOwnerViewSet)
 
 urlpatterns = router.urls
