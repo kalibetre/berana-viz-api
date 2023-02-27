4096127c1c8aa8fc8cd3540340f8622a4f4c1302
kalibetre <kalbetre@icloud.com>
2023-02-27 17:18:43 +0300

    add user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """User model
+
+    Args:
+        AbstractUser : default Django user
+    """
+    uid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    email = models.EmailField(unique=True)
