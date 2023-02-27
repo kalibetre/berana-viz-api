41296c4d084f29fe94e659ac6b129e3ac2ba5aac
kalibetre <kalbetre@icloud.com>
2023-02-28 00:49:58 +0300

    clean up comments

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from .models import User
 
+# Register User Model on Admin Site
 admin.site.register(User)
--- documents/admin.py
+++ documents/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from .models import Document
 
+# Register Document Model on Admin Site
 admin.site.register(Document)
