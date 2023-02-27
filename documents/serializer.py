4084c77ce344750bfafa6f4234e006e21c5ae47b
kalibetre <kalbetre@icloud.com>
2023-02-27 21:55:11 +0300

    add document serializer

---
+++ documents/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Document
+
+
+class DocumentSerializer(ModelSerializer):
+    """
+    A Document Serializer Class that used for serializing and deserializing of
+    Document instances
+    """
+
+    class Meta:
+        model = Document
+        fields = ('uid', 'title', 'description', 'modified', 'created',
+                  'content', 'user')
