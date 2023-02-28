2d7789c1d9b134239f24e1bb53ac87156ed1d108
kalibetre <kalbetre@icloud.com>
2023-02-28 15:45:44 +0300

    remove user related serializers, views and urls

--- beranavizapi/urls.py
+++ beranavizapi/urls.py
@@ -8,6 +8,5 @@ API_BASE_URL = "api/v1/"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(API_BASE_URL, include('core.urls')),
     path(API_BASE_URL, include('documents.urls')),
 ]
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from .views import UserViewSet
-
-router = SimpleRouter()
-router.register('users', UserViewSet)
-
-urlpatterns = router.urls
