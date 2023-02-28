3894e39d9ab1864cb11db89baf9c12b60c19bde9
kalibetre <kalbetre@icloud.com>
2023-02-28 14:59:25 +0300

    add user serializer and viewset

--- core/views.py
+++ core/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    """
+    A viewset that provides `create()`, `retrieve()`, actions for User
+    instances
+    """
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        pass
+
+    def get_serializer_context(self):
+        return {'request': self.request}
